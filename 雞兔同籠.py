115f7258e29f98e901e55f3995aef1895cc58fbb
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2021-03-08 21:35:37 +0800

    Add files via upload

--- 雞兔同籠.py
+++ 雞兔同籠.py
@@ -4,17 +4,20 @@ Created on Mon Mar  1 23:24:15 2021
 
 @author: user
 """
-
-head=int(input("heads?"))
-feat=int(input("feats?"))
-chicken=int((4*head-feat)/2)
-rabbit=int((feat-2*head)/2)
-print("There are ",chicken,end="")
-if chicken>1:
-    print(" chickens and ",rabbit,end="")
-else:
-    print(" chicken and ",rabbit,end="")
-if rabbit>1:
-    print(" rabbits.")
-else:
-    print(" rabbit.")
+while 1:
+    head=int(input("Head?"))
+    feat=int(input("Leg?"))
+    if head>=0 and feat>=0:
+        chicken=int((4*head-feat)/2)
+        rabbit=int((feat-2*head)/2)
+        print("There are ",chicken,end="")
+        if chicken>1:
+            print(" chickens and ",rabbit,end="")
+        else:
+            print(" chicken and ",rabbit,end="")
+        if rabbit>1:
+            print(" rabbits.")
+        else:
+            print(" rabbit.")
+    else:
+        print("Wrong input.")
